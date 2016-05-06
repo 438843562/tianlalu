95dd84006b34d8b41a91f18f87c7428fc58b2445
dj <dj@dengjie-pc>
2016-05-06 15:29:25 +0800

    add qq hulian

--- djproject/js/manager.js
+++ djproject/js/manager.js
@@ -2,6 +2,22 @@
 /*导航栏*/
 var daohang_left = function(){
 	$('#daohang-list').on('click', 'li', function(){
+		var leftoffset = $(this).offset().left+'px';
+		$(this).animate({right:leftoffset}, 500, function(){
+			$(this).animate({opacity:'0.5',bottom:'-23px'}, 500, function(){
+				$('#daohang-list li').each(function(){
+					if(!$(this).hasClass('active')){
+						$(this).css('opacity','1');
+						$(this).css('right','0px');
+						$(this).css('bottom','0px');
+					}
+				});
+				$('#daohang-list li').show();
+				$(this).hide();
+				var text = $(this).html();
+				$('.daohang-left .title').html(text);
+			});
+		});
 		$('#daohang-list li').removeClass('active');
 		$(this).addClass('active');
 		var subid = 'sub-'+$(this).attr('name');
@@ -36,6 +52,8 @@ var open_close = function(){
 var init = function(){
 	daohang_left();
 	open_close();
+	$('#daohang-list li').first().click();
+	
 };
 $(document).ready(init);
 })()
